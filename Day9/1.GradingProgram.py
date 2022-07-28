a7acffbc7f62749bfc3a48b7709063b76bbf5fcb
Sunil <sunilnudp@gmail.com>
2022-07-28 21:31:16 +0530

    Daily Commit

---
+++ Day9/1.GradingProgram.py
@@ -0,0 +1,37 @@
+# Write a program that **converts their scores to grades**.
+
+# This is the scoring criteria:
+# > Scores 91 - 100: Grade = "Outstanding"
+# > Scores 81 - 90: Grade = "Exceeds Expectations"
+# > Scores 71 - 80: Grade = "Acceptable"
+# > Scores 70 or lower: Grade = "Fail"
+
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades={}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+    score=student_scores[student]
+    if score>90:
+        student_grades[student] = "Outstanding"
+    elif score>80:
+        student_grades[student] = "Exceeds Expectation"
+    elif score>70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"    
+
+    
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
